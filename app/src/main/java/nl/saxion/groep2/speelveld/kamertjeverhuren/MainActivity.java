71e5337b27a9f22aaf19bbda223e8c9f6a9c5f6b
rickvanw <rickvanweersel@hotmail.com>
2016-05-18 14:25:56 +0200

    test rick

--- app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/MainActivity.java
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // just added a test comment
+        // nog een voor robert m
     }
 }
