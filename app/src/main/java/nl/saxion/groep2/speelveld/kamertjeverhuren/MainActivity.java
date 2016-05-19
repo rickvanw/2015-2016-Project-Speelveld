2450a05d97903c73895613eb0f8683574ce902af
rickvanw <rickvanweersel@hotmail.com>
2016-05-19 12:06:40 +0200

    Main with comments

--- app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/MainActivity.java
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/MainActivity.java
@@ -1,7 +1,5 @@
 package nl.saxion.groep2.speelveld.kamertjeverhuren;
 
-import android.graphics.Color;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -10,9 +8,11 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 
 import nl.saxion.groep2.speelveld.kamertjeverhuren.view.GameBoard;
-import nl.saxion.groep2.speelveld.kamertjeverhuren.view.Line;
+import nl.saxion.groep2.speelveld.kamertjeverhuren.view.LineView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int minSide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,56 +25,76 @@ public class MainActivity extends AppCompatActivity {
         // get lowest screen width or height to create a square
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        int minSide = Math.min(metrics.heightPixels, metrics.widthPixels);
+        minSide = (Math.min(metrics.heightPixels, metrics.widthPixels));
 
+        //
         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(minSide, minSide);
         this.addContentView(gameBoard, layoutParams);
 
-        drawLines();
+        // the amount of boxes on the field
+        int amountOfBoxes = 16;
+
+        // create lines on the gameboard
+        drawLines(amountOfBoxes);
 
     }
 
-    public void drawLines(){
 
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        int minSide = Math.min(metrics.heightPixels, metrics.widthPixels);
+    // drawlines method, input the amount of boxes which the field should have
+    public void drawLines(int amountOfBoxes){
 
-        int numberOfLinesHorizontalX = 3;
-        int numberOfLinesHorizontalY = 4;
+        int linesOnAxis = (int) Math.sqrt(amountOfBoxes);
 
-        int numberOfLinesVerticalX = 4;
-        int numberOfLinesVerticalY = 3;
+        // Amount of horizontal lines drawn on the x-axis
+        int numberOfLinesHorizontalX = linesOnAxis;
+        // Amount of horizontal lines drawn on the y-axis
+        int numberOfLinesHorizontalY = linesOnAxis+1;
 
-        ArrayList<Line> lines = new ArrayList<>();
+        // Amount of vertical lines drawn on the x-axis
+        int numberOfLinesVerticalX = linesOnAxis+1;
+        // Amount of vertical lines drawn on the y-axis
+        int numberOfLinesVerticalY = linesOnAxis;
 
+        // List with al the Line views
+        ArrayList<LineView> lineViews = new ArrayList<>();
+
+        // Initialise boolean to check send, this way the line view knows if it's horizontal or vertical
         boolean horizontal;
 
+        // For every horizontal line on the X axis, create a line on the Y axis
         for (int i = 0; i < numberOfLinesHorizontalX; i++) {
             horizontal = true;
             for (int j = 0; j < numberOfLinesHorizontalY; j++) {
 
+                // Create a line view
+                LineView lineView = new LineView(this, i, j, minSide, horizontal, linesOnAxis);
 
-                Line line = new Line(this, i, j, minSide, horizontal);
+                // Set the width and height of the view
+                ViewGroup.LayoutParams layoutParamsLine = new ViewGroup.LayoutParams((minSide / linesOnAxis), 20);
 
-                ViewGroup.LayoutParams layoutParamsLine = new ViewGroup.LayoutParams((minSide / 3), 20);
+                // Add the parameters to the lineView
+                this.addContentView(lineView, layoutParamsLine);
 
-                this.addContentView(line, layoutParamsLine);
+                // Save the lineview in the list
+                lineViews.add(lineView);
             }
         }
-
+        // For every vertical line on the X axis, create a line on the Y axis
         for (int i = 0; i < numberOfLinesVerticalX; i++) {
             horizontal = false;
             for (int j = 0; j < numberOfLinesVerticalY; j++) {
 
+                // Create a line view
+                LineView lineView = new LineView(this, i, j, minSide, horizontal, linesOnAxis);
 
-                Line line = new Line(this, i, j, minSide, horizontal);
+                // Set the width and height of the view
+                ViewGroup.LayoutParams layoutParamsLine = new ViewGroup.LayoutParams(20, (minSide / linesOnAxis));
 
-                ViewGroup.LayoutParams layoutParamsLine = new ViewGroup.LayoutParams(20, (minSide / 3));
+                // Add the parameters to the lineView
+                this.addContentView(lineView, layoutParamsLine);
 
-                this.addContentView(line, layoutParamsLine);
-
-                lines.add(line);
+                // Save the lineview in the list
+                lineViews.add(lineView);
             }
 
         }
