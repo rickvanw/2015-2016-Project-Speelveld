a0aeb89ad2321cb656544931aa775537396ac858
rickvanw <rickvanweersel@hotmail.com>
2016-06-08 16:31:38 +0200

    added Manual

---
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/ManualActivity.java
@@ -0,0 +1,29 @@
+package nl.saxion.groep2.speelveld.kamertjeverhuren;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ManualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual);
+
+        TextView tvManualTitle = (TextView)findViewById(R.id.tvManualTitle);
+        TextView tvManualText = (TextView)findViewById(R.id.tvManualText);
+
+        tvManualTitle.setText("Manual");
+        tvManualText.setText("dit is uitleg test test dit is uitleg test test dit is uitleg test test dit is uitleg test test");
+
+        tvManualTitle.setTextColor(Color.BLACK);
+        tvManualText.setTextColor(Color.BLACK);
+
+        tvManualTitle.setTextSize(30);
+        tvManualText.setTextSize(20);
+
+    }
+}
