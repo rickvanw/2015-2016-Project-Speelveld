25e9b6d21b995c25fe39e535cfdbe4e75ab683b1
Ruben Assink <rubenassink@gmail.com>
2016-06-09 12:51:52 +0200

    options

---
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/model/Options.java
@@ -0,0 +1,39 @@
+package nl.saxion.groep2.speelveld.kamertjeverhuren.model;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import nl.saxion.groep2.speelveld.kamertjeverhuren.GameActivity;
+import nl.saxion.groep2.speelveld.kamertjeverhuren.ManualActivity;
+
+/**
+ * Created by rubenassink on 08-06-16.
+ */
+public class Options extends DialogFragment {
+
+    String[] Options = {"Mute", "Restart", "Manual", "Settings"};
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setItems(Options, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case 0: AudioPlay.muteUnmute(); break;
+                            case 1: // callback + restart game ;break;
+                            case 2: // callback + intent manual;break;
+                            case 3: // callback + intent settings; break;
+                        }
+
+
+                    }
+                });
+        return builder.create();
+    }
+}
