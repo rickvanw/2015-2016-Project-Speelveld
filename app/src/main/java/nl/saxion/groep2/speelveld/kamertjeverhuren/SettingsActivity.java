6c98120f06958734dd294896fe706fb98afb3ad0
Ruben Assink <rubenassink@gmail.com>
2016-06-10 11:57:07 +0200

    Dialog fragment intents implemented

--- app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/SettingsActivity.java
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/SettingsActivity.java
@@ -3,8 +3,6 @@ package nl.saxion.groep2.speelveld.kamertjeverhuren;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
 
 /**
  * Created by rubenassink on 10-06-16.
