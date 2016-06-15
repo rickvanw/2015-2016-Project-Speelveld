21ad1fa57ff01cb56de50fd6b399c0bfe171da2d
robertm93 <no-mail@example.com>
2016-06-15 15:28:09 +0200

    code cleanup manual

--- app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/ManualActivity.java
+++ app/src/main/java/nl/saxion/groep2/speelveld/kamertjeverhuren/ManualActivity.java
@@ -1,10 +1,7 @@
 package nl.saxion.groep2.speelveld.kamertjeverhuren;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class ManualActivity extends AppCompatActivity {
 
@@ -12,22 +9,5 @@ public class ManualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
-
-        TextView tvManualTitle = (TextView) findViewById(R.id.tvManualTitle);
-        TextView tvManualText = (TextView) findViewById(R.id.tvManualText);
-        ImageView ivManual1 = (ImageView) findViewById(R.id.ivManual1);
-        ImageView ivManual2 = (ImageView) findViewById(R.id.ivManual2);
-
-        ivManual1.setImageResource(R.drawable.manual_place_lines);
-        ivManual2.setImageResource(R.drawable.manual_box_done);
-
-        tvManualTitle.setText("Manual");
-        tvManualText.setText("The players have to take turns in tapping a line." + "\n" + "\n" + "Once the last line of a box is tapped, the point belongs to the player that placed the last line. " + "\n" + "\n" + "If a player has claimed a box, the same player can place another line.");
-
-        tvManualTitle.setTextColor(Color.BLACK);
-        tvManualText.setTextColor(Color.BLACK);
-
-        tvManualTitle.setTextSize(30);
-        tvManualText.setTextSize(20);
     }
 }
